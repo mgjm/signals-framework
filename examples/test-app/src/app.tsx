2d0049ca12f07ab7e1b9d2dd515c93d3b2a0f897
Martin Michaelis <code@mgjm.de>
2025-01-28 14:54:12 +0000

    Change example to simple todo app

--- examples/test-app/src/app.tsx
+++ examples/test-app/src/app.tsx
@@ -1,52 +1,83 @@
-import { SignalArray } from "signal-utils/array";
-import { For,  Show, createEffect, onCleanup } from "signals-framework";
+import { For, Show, createEffect } from "signals-framework";
 
-import styles from './app.module.css';
+type TodoItem = { title: string; done: boolean };
 
-function Loop({values}: {
-  values: Signal<number[]>,
-}) {
-  return <For each={values}>
-    {(item, index) => <>{index}: {item}<br/></>}
-  </For>
-}
+export function App()  {
+  const newTitle = new Signal.State("");
+  const todos = new Signal.State<TodoItem[]>(JSON.parse(localStorage.getItem('todos') || '[]'));
 
-export function App({reset}: {
-  reset: () =>void,
-}) {
-  const signal = new Signal.State('htpps://mgjm.dev');
-  const counter = new Signal.State(0);
   createEffect(() => {
-    console.log('counter changed:', counter.get());
+    localStorage.setItem('todos', JSON.stringify(todos.get()));
   });
-  const values = new SignalArray<number>([]);
-  const values2 = new Signal.State<number[]>([]);
-  const values3 = new Signal.State<number[]>([]);
-  const interval = setInterval(() => {
-    counter.set(counter.get() + 1);
-    for(let i = 0; i < values.length; i++) {
-      values[i]--;
-    }
-    values.push(counter.get());
-    values2.set([...values2.get().map(v => v -1), counter.get()]);
-    const v3 = [counter.get(), ...values3.get()];
-    v3.push(...v3.splice((Math.random() * v3.length )| 0, 1));
-    values3.set(v3);
-  }, 1000);
-  onCleanup(() => {
-    clearInterval(interval);
-  });
-  const even = new Signal.Computed(() => counter.get() % 3 === 0 ? false : counter.get() );
-  return <>
-      <a href={signal}>Home</a>
-      <hr />
-      <h1>Signals Framework</h1>
-      <div>Counter: {counter}</div>
-      <br/>
-      <Show when={even}>
-        <Loop values={values3} />
-      </Show>
-      <button className={styles.button} on:click={reset}>Close</button>
-    </>;
-}
 
+  function addTodo(e: SubmitEvent)  {
+    e.preventDefault();
+    todos.set([...todos.get(), {
+      title: newTitle.get(),
+      done: false,
+    }]);
+    newTitle.set("");
+  }
+
+  function changeTodos(cb: (todos: TodoItem[]) => void) {
+    const newTodos = [...todos.get()];
+    cb(newTodos);
+    todos.set(newTodos)
+  }
+
+  function changeTodo(i: number, todo: Partial<TodoItem>) {
+    changeTodos(todos => {
+      todos.splice(i, 1, {
+        ...todos[i],
+        ...todo,
+      });
+    });
+  }
+
+  const length = new Signal.Computed(() => todos.get().length);
+  const isEmpty = new Signal.Computed(() => todos.get().length === 0);
+
+  return (
+    <>
+      <h3>Simple Todos Example</h3>
+      <form on:submit={addTodo}>
+        <input
+          placeholder="enter todo and click +"
+          required
+          value={newTitle}
+          on:input={(e) => newTitle.set((e.currentTarget as any).value)}
+        />
+        <button>+</button>
+      </form>
+      <p>{length} Todos:</p>
+      <Show when={isEmpty}>
+        <p>No Todos yet</p>
+      </Show>
+      <For each={todos}>
+        {(todo, i) => (
+          <div>
+            <input
+              type="checkbox"
+              checked={todo.done}
+              on:change={(e) => changeTodo(i.get(), {
+                done: (e.currentTarget as any).checked,
+              })}
+            />
+            <input
+              type="text"
+              value={todo.title}
+              on:change={(e) => changeTodo(i.get(), {
+                title: (e.currentTarget as any).value,
+              })}
+            />
+            <button on:click={() => changeTodos(todos => {
+              todos.splice(i.get(), 1);
+            })}>
+              x
+            </button>
+          </div>
+        )}
+      </For>
+    </>
+  );
+}
